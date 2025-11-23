565bad3ca0dd9202b8b84823ab2e7e36a11eb974
Gabriel Oliveira <146120482+rnxgabriel@users.noreply.github.com>
2025-11-23 19:16:37 -0300

    Implementa NoteRepositoryImpl e configura VCS
    - Adiciona a classe `NoteRepositoryImpl` implementando a interface `NoteRepository`.
    - Implementa métodos para obter notas, buscar por ID, inserir e deletar usando `NoteDao`.
    - Adiciona configuração de mapeamento de diretório do Git em `.idea/vcs.xml`.
    
    Signed-off-by: Gabriel Oliveira <146120482+rnxgabriel@users.noreply.github.com>

---
+++ app/src/main/java/com/gabrielaltruist/noteapp/feature_note/data/repository/NoteRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.gabrielaltruist.noteapp.feature_note.data.repository
+
+import com.gabrielaltruist.noteapp.feature_note.data.source.NoteDao
+import com.gabrielaltruist.noteapp.feature_note.domain.model.Note
+import com.gabrielaltruist.noteapp.feature_note.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepositoryImpl(
+    private val dao: NoteDao
+): NoteRepository {
+    override fun getNotes(): Flow<List<Note>> {
+        return dao.getNotes()
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        return dao.getNoteById(id)
+    }
+
+    override suspend fun insertNote(note: Note) {
+        dao.insertNote(note)
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        dao.deleteNote(note)
+    }
+}
