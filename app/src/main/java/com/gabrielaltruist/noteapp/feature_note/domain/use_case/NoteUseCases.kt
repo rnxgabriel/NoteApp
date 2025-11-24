358c5441f67199b6a38724671da0477f90c1bde1
Gabriel Oliveira <146120482+rnxgabriel@users.noreply.github.com>
2025-11-24 16:29:53 -0300

    Adiciona NotesState e NotesEvent para a camada de apresentação
    - Cria a classe `NotesEvent` para representar interações do usuário (ordenação, exclusão, restauração e alternância de seção).
    - Cria a data class `NotesState` para gerenciar o estado da UI, incluindo a lista de notas, ordenação atual e visibilidade da seção de ordem.
    
    Signed-off-by: Gabriel Oliveira <146120482+rnxgabriel@users.noreply.github.com>

--- app/src/main/java/com/gabrielaltruist/noteapp/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/gabrielaltruist/noteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -3,4 +3,5 @@ package com.gabrielaltruist.noteapp.feature_note.domain.use_case
 data class NoteUseCases(
     val getNotes: GetNoteUseCase,
     val deleteNote: DeleteNoteUseCase,
+    val addNoteUseCase: AddNoteUseCase
 )
---
+++ app/src/main/java/com/gabrielaltruist/noteapp/feature_note/presentation/NotesViewModel.kt
@@ -0,0 +1,80 @@
+package com.gabrielaltruist.noteapp.feature_note.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gabrielaltruist.noteapp.feature_note.domain.model.Note
+import com.gabrielaltruist.noteapp.feature_note.domain.use_case.NoteUseCases
+import com.gabrielaltruist.noteapp.feature_note.domain.util.NoteOrder
+import com.gabrielaltruist.noteapp.feature_note.domain.util.OrderType
+import com.gabrielaltruist.noteapp.feature_note.presentation.util.NotesEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NotesViewModel @Inject constructor(
+    private val noteUseCases: NoteUseCases
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(NotesState())
+    val state: StateFlow<NotesState> = _state.asStateFlow()
+
+    private var recentlyDeletedNote: Note? = null
+
+    private var getNotesJob: Job? = null
+
+    init {
+        getNotes(NoteOrder.Date(OrderType.Descending))
+    }
+
+    fun onEvent(event: NotesEvent) {
+        when (event) {
+
+            is NotesEvent.Order -> {
+                if (state.value.noteOrder::class == event.noteOrder::class &&
+                    state.value.noteOrder.orderType == event.noteOrder.orderType
+                ) {
+                    return
+                }
+
+            }
+
+            is NotesEvent.DeleteNote -> {
+                viewModelScope.launch {
+                    noteUseCases.deleteNote(event.note)
+                    recentlyDeletedNote = event.note
+                }
+            }
+
+            is NotesEvent.RestoreNote -> {
+                viewModelScope.launch {
+                    noteUseCases.addNoteUseCase(recentlyDeletedNote ?: return@launch)
+                    recentlyDeletedNote = null
+                }
+            }
+
+            is NotesEvent.ToggleOrderSection -> {
+                _state.value = state.value.copy(
+                    isOrderSectionVisible = !state.value.isOrderSectionVisible
+                )
+            }
+        }
+    }
+
+    private fun getNotes(noteOrder: NoteOrder) {
+        getNotesJob?.cancel()
+        getNotesJob = noteUseCases.getNotes(noteOrder).onEach { notes ->
+            _state.value = state.value.copy(
+                notes = notes,
+                noteOrder = noteOrder
+            )
+        }
+            .launchIn(viewModelScope)
+    }
+}
