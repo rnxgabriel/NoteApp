ce3b0ea1f092aabbbc25f8fa20806131152c386c
Gabriel Oliveira <146120482+rnxgabriel@users.noreply.github.com>
2025-11-24 14:06:10 -0300

    Implementa GetNotesUseCase e utilitários de ordenação
    - Adiciona `GetNotesUseCase` com lógica para buscar e ordenar notas via `NoteRepository`.
    - Introduz as classes `NoteOrder` e `OrderType` para suportar ordenação por título, data e cor (ascendente/descendente).
    - Atualiza configurações do projeto em `.idea/gradle.xml` e `.idea/misc.xml`.
    
    Signed-off-by: Gabriel Oliveira <146120482+rnxgabriel@users.noreply.github.com>

---
+++ app/src/main/java/com/gabrielaltruist/noteapp/feature_note/domain/use_case/GetNotesUseCase.kt
@@ -0,0 +1,38 @@
+package com.gabrielaltruist.noteapp.feature_note.domain.use_case
+
+import com.gabrielaltruist.noteapp.feature_note.domain.model.Note
+import com.gabrielaltruist.noteapp.feature_note.domain.repository.NoteRepository
+import com.gabrielaltruist.noteapp.feature_note.domain.util.NoteOrder
+import com.gabrielaltruist.noteapp.feature_note.domain.util.OrderType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class GetNotesUseCase(
+    private val repository: NoteRepository
+) {
+
+    operator fun invoke(
+        noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending)
+    ): Flow<List<Note>> {
+
+        return repository.getNotes().map { notes ->
+            when (noteOrder.orderType) {
+                OrderType.Ascending -> {
+                    when (noteOrder) {
+                        is NoteOrder.Date -> notes.sortedBy { it.timestamp }
+                        is NoteOrder.Title -> notes.sortedBy { it.title.lowercase() }
+                        is NoteOrder.Color -> notes.sortedBy { it.color }
+                    }
+                }
+
+                OrderType.Descending -> {
+                    when (noteOrder) {
+                        is NoteOrder.Date -> notes.sortedByDescending { it.timestamp }
+                        is NoteOrder.Title -> notes.sortedByDescending { it.title.lowercase() }
+                        is NoteOrder.Color -> notes.sortedByDescending { it.color }
+                    }
+                }
+            }
+        }
+    }
+}
