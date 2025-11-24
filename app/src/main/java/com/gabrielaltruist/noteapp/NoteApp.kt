3704d4962196dd6487f257161bfbe9fca39331de
Gabriel Oliveira <146120482+rnxgabriel@users.noreply.github.com>
2025-11-24 14:28:51 -0300

    Refatora use cases e adiciona constante de banco de dados
    - Renomeia `GetNotesUseCase` para `GetNoteUseCase`.
    - Renomeia `NotesUseCases` para `NoteUseCases` e atualiza suas referências.
    - Adiciona constante `DATABASE_NAME` em `NoteDatabase`.
    
    Signed-off-by: Gabriel Oliveira <146120482+rnxgabriel@users.noreply.github.com>

---
+++ app/src/main/java/com/gabrielaltruist/noteapp/NoteApp.kt
@@ -0,0 +1,7 @@
+package com.gabrielaltruist.noteapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteApp : Application() {}
---
+++ app/src/main/java/com/gabrielaltruist/noteapp/core/di/AppModule.kt
@@ -0,0 +1,46 @@
+package com.gabrielaltruist.noteapp.core.di
+
+import android.app.Application
+import androidx.room.Room
+import com.gabrielaltruist.noteapp.feature_note.data.repository.NoteRepositoryImpl
+import com.gabrielaltruist.noteapp.feature_note.data.source.NoteDatabase
+import com.gabrielaltruist.noteapp.feature_note.domain.repository.NoteRepository
+import com.gabrielaltruist.noteapp.feature_note.domain.use_case.DeleteNoteUseCase
+import com.gabrielaltruist.noteapp.feature_note.domain.use_case.GetNoteUseCase
+import com.gabrielaltruist.noteapp.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNoteUseCase(repository),
+            deleteNote = DeleteNoteUseCase(repository)
+        )
+
+    }
+}
